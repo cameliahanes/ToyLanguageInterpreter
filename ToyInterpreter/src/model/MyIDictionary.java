6eff557dae33127ed6d6718da153516d7b40a16d
Camelia-Andreea Hanes <camelia_hanes@yahoo.ro>
2017-12-13 15:49:22 +0200

    Update MyIDictionary.java

--- ToyInterpreter/src/model/MyIDictionary.java
+++ ToyInterpreter/src/model/MyIDictionary.java
@@ -1,10 +1,14 @@
 package model;
 
 import java.util.Collection;
+import java.util.HashMap;
+
 
 public interface MyIDictionary<K, V> {
-	V get( K key );
-	V put( K key, V val );
-	Collection<V> values();
-	V remove(K fd);
+    V get( K key );
+    V put( K key, V val );
+    Collection<V> values();
+    V remove(K fd);
+    HashMap<K, V> toHashMap();
+    MyIDictionary<K, V> clone();
 }
